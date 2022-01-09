5e9cb84380340962f239951e8517b31d6690115d
lbabysharkl <first.tanapol@gmail.com>
2022-01-09 21:35:54 +0700

    update final solve.py

--- algo/solve.py
+++ algo/solve.py
@@ -1,12 +1,12 @@
-def solve(wordList, target) :
-    for i in range(len(wordList)) :
-        for j in range(i+1,len(wordList)) : 
-            if wordList[i]+wordList[j] == target :
-                return (wordList[i],wordList[j])
-    return
+import bisect
+
+def solve(wordList,target) :
+    wordList.sort()
+    for i in wordList :
+        tmp = target[len(i):]
+        if wordList[bisect.bisect_left(wordList,tmp)] == tmp :
+            return (i,tmp)
 
 wordList = input("wordList :").split()
 target = input("target :")
 print(solve(wordList, target))
-
-
